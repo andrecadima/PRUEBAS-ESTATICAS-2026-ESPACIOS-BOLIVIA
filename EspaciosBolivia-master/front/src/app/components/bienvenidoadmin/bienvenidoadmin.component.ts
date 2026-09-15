42e35537c5403eed3c02e76ac2f66711270964f7
Camila <camila.carvajal@ucb.edu.bo>
2026-09-15 12:07:32 -0400

    fix: Member 'authservice: AuthService' is never reassigned; marked it as readonly

--- EspaciosBolivia-master/front/src/app/components/bienvenidoadmin/bienvenidoadmin.component.ts
+++ EspaciosBolivia-master/front/src/app/components/bienvenidoadmin/bienvenidoadmin.component.ts
@@ -41,7 +41,7 @@ export class BienvenidoadminComponent implements OnInit {
     private apiService: ApiService,
     private route: ActivatedRoute,
     private router: Router,
-    private authservice: AuthService
+    private readonly authservice: AuthService
   ) {}
 
   ngOnInit(): void {
