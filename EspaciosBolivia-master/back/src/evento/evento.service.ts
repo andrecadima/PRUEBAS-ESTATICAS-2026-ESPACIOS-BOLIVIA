a18353c7a889bcb9a527f7fcb8af9b0f17db240f
Camila <camila.carvajal@ucb.edu.bo>
2026-09-15 13:29:48 -0400

    fix: Member 'espacioRepository' is never reassigned; marked it as readonly.

--- EspaciosBolivia-master/back/src/evento/evento.service.ts
+++ EspaciosBolivia-master/back/src/evento/evento.service.ts
@@ -12,7 +12,7 @@ export class EventoService
 
   private eventoRepository;
   private usuarioRepository;
-  private espacioRepository;
+  private readonly espacioRepository;
 
   constructor(private dataSource: DataSource) 
   {
