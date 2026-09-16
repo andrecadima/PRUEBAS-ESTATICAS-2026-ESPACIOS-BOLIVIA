06224d40735d420dfc2fbd0b4caa23bea23185bf
Camila <camila.carvajal@ucb.edu.bo>
2026-09-16 11:44:32 -0400

    fix:Member 'eventoRepository' is never reassigned; marked it as readonly

--- EspaciosBolivia-master/back/src/espacio/espacio.service.ts
+++ EspaciosBolivia-master/back/src/espacio/espacio.service.ts
@@ -8,9 +8,9 @@ import { EventoEntity } from 'src/evento/evento.entity';
 export class EspacioService {
 
   private espacioRepository;
-  private eventoRepository;
+  private readonly eventoRepository;
 
-  constructor(private dataSource: DataSource) 
+  constructor(private readonly dataSource: DataSource) 
   {
     this.espacioRepository = this.dataSource.getRepository(EspacioEntity);
     this.eventoRepository = this.dataSource.getRepository(EventoEntity);
