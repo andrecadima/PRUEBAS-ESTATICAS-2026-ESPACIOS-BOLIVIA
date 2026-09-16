8d3676d965371ff222899b0359ca7a966b50aaaf
Camila <camila.carvajal@ucb.edu.bo>
2026-09-16 19:40:34 -0400

    fix: @angular/forms' imported multiple times

--- EspaciosBolivia-master/front/src/app/app.module.ts
+++ EspaciosBolivia-master/front/src/app/app.module.ts
@@ -1,23 +1,18 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AuthService } from './services/auth.service';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { DropdownModule } from 'primeng/dropdown';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { OrderListModule } from 'primeng/orderlist';
 import { MapaeventosModule } from './mapaeventos/mapaeventos.module';
-import { FormsModule } from '@angular/forms';
 
 // Importa tu componente
 import { VerEventoPresiComponent } from './components/ver-evento-presi/ver-evento-presi.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    VerEventoPresiComponent  // Asegúrate de declarar tu componente aquí
-  ],
   imports: [
     BrowserModule,
     HttpClientModule,
@@ -25,10 +20,12 @@ import { VerEventoPresiComponent } from './components/ver-evento-presi/ver-event
     BrowserAnimationsModule,
     DropdownModule,
     OrderListModule,
-    MapaeventosModule,
-    FormsModule
+    VerEventoPresiComponent
   ],
-  providers: [AuthService],
-  bootstrap: [AppComponent]
+  providers: [AuthService]
 })
 export class AppModule {}
+
+bootstrapApplication(AppComponent, {
+  providers: [AuthService]
+});
