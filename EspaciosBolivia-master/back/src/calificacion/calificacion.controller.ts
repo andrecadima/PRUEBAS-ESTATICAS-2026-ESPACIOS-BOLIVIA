688a44de54251d05effad22e9f31b15edbe296a9
Camila <camila.carvajal@ucb.edu.bo>
2026-09-18 10:56:26 -0400

    fix: error in handle eception calificacion controller

--- EspaciosBolivia-master/back/src/calificacion/calificacion.controller.ts
+++ EspaciosBolivia-master/back/src/calificacion/calificacion.controller.ts
@@ -7,54 +7,44 @@ import { DataSource } from 'typeorm';
 import { TimeoutError } from 'rxjs';
 
 @Controller('calificacion')
-export class CalificacionController 
-{
-   
+export class CalificacionController {
+
    private readonly eventoRepository;
-   constructor(private readonly calificacionservice: CalificacionService,private readonly dataSource: DataSource)
-   {
-      
-      this.eventoRepository=this.dataSource.getRepository(EventoEntity);
+   constructor(private readonly calificacionservice: CalificacionService, private readonly dataSource: DataSource) {
+
+      this.eventoRepository = this.dataSource.getRepository(EventoEntity);
    }
 
    @Post()
-   async create(@Body() dto1:CalificacionDto1)
-   {
-      try{
-         const evento = await this.eventoRepository.findOneBy({nombre:dto1.id_evento});
-         if(!evento)
-         {
+   async create(@Body() dto1: CalificacionDto1) {
+      try {
+         const evento = await this.eventoRepository.findOneBy({ nombre: dto1.id_evento });
+         if (!evento) {
             throw new BadRequestException(`El evento no existe`);
          }
-         const dto: CalificacionDto=
+         const dto: CalificacionDto =
          {
             id_evento: evento.id,
             calificacion: dto1.calificacion,
             comentario: dto1.comentario
          };
-         return await  this.calificacionservice.create(dto);
-      } catch (error) 
-      {
-         if (error instanceof TimeoutError) 
-         {  // Verifica si el error es por tiempo de espera
+         return await this.calificacionservice.create(dto);
+      } catch (error) {
+         if (error instanceof TimeoutError) {  // Verifica si el error es por tiempo de espera
             throw new RequestTimeoutException('La conexión con la base de datos está tardando demasiado. Intenta más tarde.');
          }
          // Si es otro tipo de error, lanzamos un error interno
          console.error('Error en create:', error); // Esto te dará más información
-           throw error; 
+         throw error;
       }
    }
 
    @Get('todas-calificaciones')
-   async todascalif()
-   {
-      try
-      {
+   async todascalif() {
+      try {
          return await this.calificacionservice.califs();
-      } catch (error) 
-      {
-         if (error instanceof TimeoutError) 
-         {  // Verifica si el error es por tiempo de espera
+      } catch (error) {
+         if (error instanceof TimeoutError) {  // Verifica si el error es por tiempo de espera
             throw new RequestTimeoutException('La conexión con la base de datos está tardando demasiado. Intenta más tarde.');
          }
          // Si es otro tipo de error, lanzamos un error interno
@@ -63,45 +53,46 @@ export class CalificacionController
    }
 
    @Get('calificaciones-evento')
-   async califsevento(@Body() body: {nomevento:string})
-   {
-      try
-      {
-         const {nomevento} = body;
+   async califsevento(@Body() body: { nomevento: string }) {
+      try {
+         const { nomevento } = body;
          return await this.calificacionservice.califsevento(nomevento);
-      } catch (error) 
-      {
-         if (error instanceof TimeoutError) 
-         {  // Verifica si el error es por tiempo de espera
+      } catch (error) {
+         if (error instanceof TimeoutError) {  // Verifica si el error es por tiempo de espera
             throw new RequestTimeoutException('La conexión con la base de datos está tardando demasiado. Intenta más tarde.');
          }
          // Si es otro tipo de error, lanzamos un error interno
          throw new InternalServerErrorException('Hubo un problema al crear el espacio. Intenta más tarde.');
       }
    }
-
-      @Get('promedio-evento')
-      async promedioEvento(@Body() body:{nomevento:string})
-      {
-         const {nomevento}=body;
-         return await this.calificacionservice.promedioCalificacionesEvento(nomevento);
+   @Get('calificaciones-desc')
+   async calificacionesDesc() {
+      try {
+         const resultado = await this.calificacionservice.calificacionesOrdenadasDesc();
+         return resultado;
+      } catch (error) {
+         console.error('Error al obtener las calificaciones: ', error);
+         throw new InternalServerErrorException('Error al obtener las calificaciones ordenadas');
       }
-      @Get('promedios-eventos')
-      async promediosEventos()
-      {
-         return await this.calificacionservice.listaPromedioEventos();
-      }
-      @Get('total-calificaciones-evento')
-      async totalCalificaciones(@Body() body:{nomevento:string})
-      {
-         const {nomevento}=body;
-         return await this.calificacionservice.totalCalificacionesEvento(nomevento);
-      }
-      @Get('top-eventos')
-      async topEventis(@Body()body:{cantidad:number})
-      {
-         const{cantidad}=body;
-         return await this.calificacionservice.topEventos(cantidad);
-      }
+   }
+   @Get('promedio-evento')
+   async promedioEvento(@Body() body: { nomevento: string }) {
+      const { nomevento } = body;
+      return await this.calificacionservice.promedioCalificacionesEvento(nomevento);
+   }
+   @Get('promedios-eventos')
+   async promediosEventos() {
+      return await this.calificacionservice.listaPromedioEventos();
+   }
+   @Get('total-calificaciones-evento')
+   async totalCalificaciones(@Body() body: { nomevento: string }) {
+      const { nomevento } = body;
+      return await this.calificacionservice.totalCalificacionesEvento(nomevento);
+   }
+   @Get('top-eventos')
+   async topEventis(@Body() body: { cantidad: number }) {
+      const { cantidad } = body;
+      return await this.calificacionservice.topEventos(cantidad);
+   }
 
 }
